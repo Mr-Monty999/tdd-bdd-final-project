e4c0110d48e8489af75e5d0f356528618cc44c79
Mr-Monty999 <montser.mohmed2022@gmail.com>
2024-04-10 01:30:32 -0400

    fix factories bugs

--- tests/test_models.py
+++ tests/test_models.py
@@ -104,3 +104,9 @@ class TestProductModel(unittest.TestCase):
     #
     # ADD YOUR TEST CASES HERE
     #
+
+    def test_read_product(self):
+        product = ProductFactory()
+        self.assertEqual(True,True)
+
+
